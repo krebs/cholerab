160c9da99f9bd4e969476ab9b5c19b02b0449b38
lassulus <git@lassul.us>
2018-05-09 22:41:29 +0200

    add default.nix

---
+++ default.nix
@@ -0,0 +1,11 @@
+{ nixpkgs ? import <nixpkgs> {}, ... }:
+with nixpkgs;
+stdenv.mkDerivation {
+  name = "thesauron";
+  src = ./thesauron.md;
+  phases = [ "buildPhase" ];
+  buildPhase = ''
+    mkdir -p $out/share/man/man1
+    ${pandoc}/bin/pandoc -s -t man $src -o $out/share/man/man1/thesauron.1
+  '';
+}
