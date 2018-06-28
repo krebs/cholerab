9c0a1a7e5b4ba19e96b93295693920c25b0f6a5f
lassulus <lassulus@lassul.us>
2018-06-28 12:58:33 +0200

    ENTERPRISE_PATTERN -> enterprise-patterns.md

--- default.nix
+++ default.nix
@@ -1,10 +1,11 @@
 { nixpkgs ? import <nixpkgs> {}, ... }: with nixpkgs;
 stdenv.mkDerivation {
-  name = "thesauron";
-  src = ./thesauron.md;
+  name = "cholerab";
+  src = ./.;
   phases = [ "buildPhase" ];
   buildPhase = ''
     mkdir -p $out/share/man/man1
-    ${pandoc}/bin/pandoc -s -t man $src -o $out/share/man/man1/thesauron.1
+    ${pandoc}/bin/pandoc -s -t man $src/thesauron.md -o $out/share/man/man1/thesauron.1
+    ${pandoc}/bin/pandoc -s -t man $src/enterprise-patterns.md -o $out/share/man/man1/enterprise-patterns.1
   '';
 }
