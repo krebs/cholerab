32e57cba2653d90d120edd3984bd1ae924995653
Kierán Meinhardt <kieran.meinhardt@gmail.com>
2019-01-15 23:37:25 +0100

    Convert to AsciiDoc

--- default.nix
+++ default.nix
@@ -4,8 +4,10 @@ stdenv.mkDerivation {
   src = ./.;
   phases = [ "buildPhase" ];
   buildPhase = ''
-    mkdir -p $out/share/man/man1
-    ${pandoc}/bin/pandoc -s -t man $src/thesauron.md -o $out/share/man/man1/thesauron.1
-    ${pandoc}/bin/pandoc -s -t man $src/enterprise-patterns.md -o $out/share/man/man1/enterprise-patterns.1
+    mkdir -p $out/share/man/man1 $out/share/doc
+    ${asciidoctor}/bin/asciidoctor $src/thesauron.adoc -o $out/share/doc/thesauron.html
+    ${asciidoctor}/bin/asciidoctor $src/enterprise-patterns.adoc -o $out/share/doc/enterprise-patterns.html
+    ${asciidoctor}/bin/asciidoctor -b manpage $src/thesauron.adoc -o $out/share/man/man1/thesauron.1
+    ${asciidoctor}/bin/asciidoctor -b manpage $src/enterprise-patterns.adoc -o $out/share/man/man1/enterprise-patterns.1
   '';
 }
